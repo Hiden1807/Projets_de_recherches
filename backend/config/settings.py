7b02f54f5549b058167a37f3945e8bf2ba0e29fc
Hiden1807 <ngalamulumejonas18@gmail.com>
2026-07-20 00:28:29 +0100

    Re-ajout config Cloudinary (DEFAULT_FILE_STORAGE) perdue par erreur

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -249,3 +249,10 @@ LOGGING = {
     "handlers": {"console": {"class": "logging.StreamHandler"}},
     "root": {"handlers": ["console"], "level": "INFO"},
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
